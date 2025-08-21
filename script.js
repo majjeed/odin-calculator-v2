9715215af38ebcba74a6087df228805b4c108562
majjeed <34240451+majjeed@users.noreply.github.com>
2025-08-21 15:17:34 +0100

    add inital logic flow

--- script.js
+++ script.js
@@ -27,3 +27,32 @@ function operate(operator, a, b) {
       return divide(a, b);
   }
 }
+
+let firstOperand = null;
+let secondOperand = null;
+let currentOperator = null;
+let shouldResetDisplay = null;
+
+let btnNodeList = document.querySelectorAll("button");
+
+for (btn of btnNodeList) {
+  btn.addEventListener("click", (e) => console.log(e.target.textContent));
+}
+
+function logicFlow(target) {
+  // check if event target is a number or an operator
+  if (target.className === "digit") {
+    // do something for digits
+    if (firstOperand === null) {
+    } else {
+      //no need to check for second operand
+    }
+  } else if (target.className === "operator") {
+    //do something else for operators
+    if (currentOperator === null) {
+      // do something
+    } else {
+      // calculate something
+    }
+  }
+}
